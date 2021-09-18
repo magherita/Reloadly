7ddc2c87d5971ae9b654ae118edb4eac73b37087
Daniel Okello <handsdani@gmail.com>
2021-09-19 00:30:39 +0300

    Set classes to be worked upon in the application Layer

---
+++ Application/Donations/DonationService.cs
@@ -0,0 +1,7 @@
+namespace Application.Donations
+{
+    public class DonationService
+    {
+        
+    }
+}
---
+++ Application/Donations/IDonationService.cs
@@ -0,0 +1,7 @@
+namespace Application.Donations
+{
+    public interface IDonationService
+    {
+        
+    }
+}
