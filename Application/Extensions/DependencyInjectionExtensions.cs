c79fb0bd77e30f296e5beab7a2877a7d7499f579
Daniel Okello <handsdani@gmail.com>
2021-09-20 00:30:42 +0300

    Service layer extension sorted

--- Application/Extensions/DependencyInjectionExtensions.cs
+++ Application/Extensions/DependencyInjectionExtensions.cs
@@ -1,7 +1,21 @@
+using Application.Donations;
+using Application.Users;
+using Application.Wallets;
+using Domain.Donation;
+using Domain.Wallet;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Application.Extensions
 {
-    public class DependencyInjectionExtensions
+    public static class DependencyInjectionExtensions
     {
-        
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IWalletService, WalletService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IDonationService, DonationService>();
+
+            return services;
+        }
     }
 }
